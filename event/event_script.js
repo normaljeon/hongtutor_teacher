2b3949ce407d1c15bc0f1068da902847c91bbd00
inputjsh <inputjsh@gmail.com>
2021-12-17 18:01:37 +0900

    선생님 피드백을 통해, 완성함 내가 몰랐던 부분 3가지 : (1) 이벤트추가시, type : 'mousemove'은  document에 걸자 (①document.addEventListener('mousemove', resize), ②document.removeEventListener('mousemove', resize)) (2) listener 등록할 때, 함수는 ()없이 함수명만 넣고, (3) 함수는 파라미터로 event 를 넣어야 함 const resize = (event) => { ...let resize_col = event.x - 5; .... }

--- event/event_script.js
+++ event/event_script.js
@@ -12,24 +12,46 @@ const element_rightSide = document.getElementById('right-side');
 /*      왼쪽면      |          오른쪽면      */
 /*    width 감소    | width 증가 , left 감소 */
 
-const resize = (resize_col) => {
-  element_leftSide.style.width = `${resize_col}px`;
+const resize = (event) => {
+  // let resize_col = event.x;
+  // element_leftSide.style.width = `${resize_col}px`;
+  // element_verticalSidebar.style.left = `${resize_col}px`;
+  // element_rightSide.style.left = `${resize_col + 10}px`;
+  // element_rightSide.style.width = `calc(100% - ${resize_col + 10}px)`;
+
+  // 괄호가 풀림
+  // element_leftSide.style.width = (event.x - 5) + 'px';
+
+  // element_verticalSidebar.style.left = event.x + 'px';
+  // element_rightSide.style.left = event.x + 10 + 'px';
+  // element_rightSide.style.width = `calc(100% - ${event.x + 10}px)`;
+
+  let resize_col = event.x - 5;
+  element_leftSide.style.width = `${event.x}px`;
   element_verticalSidebar.style.left = `${resize_col}px`;
   element_rightSide.style.left = `${resize_col + 10}px`;
   element_rightSide.style.width = `calc(100% - ${resize_col + 10}px)`;
 };
 
-let startX = 3;
+// const resize = (event) => {
+//   element_rightSide.style.left = event.x + 'px';
+//   element_leftSide.style.width = event.x + 'px';
+//   element_rightSide.style.width = (100 % -event.x) + 'px';
+// };
 
-element_verticalSidebar.addEventListener('mousedown', (e) => {
-  console.log(e.clientX);
+// element_verticalSidebar.addEventListener('mousedown', (e) => {
+//   document.addEventListener('mousemove', (e) => {
+//     resize(e.pageX - 5);
+//   });
+//   document.addEventListener('mouseup', () => {
+//     document.removeEventListener('mousemove', resize);
+//   });
+// });
 
-  element_verticalSidebar.addEventListener('mousemove', (e) => {
-    console.log('//// pageX ////');
-    console.log(e.clientX);
-
-    // -5는 중간값이 필요할 것 같아 넣었는데요. 사실 이게 맞는지 잘 모르겠습니다 ㅠㅠ
-    resize(e.pageX - 5);
+element_verticalSidebar.addEventListener('mousedown', () => {
+  document.addEventListener('mousemove', resize);
+  element_verticalSidebar.addEventListener('mouseup', () => {
+    document.removeEventListener('mousemove', resize);
   });
 });
 
